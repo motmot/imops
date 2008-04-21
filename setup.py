f26c1c70704ab0701a7a8197012cb4dec5979c0b
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-04-21 02:45:01 +0000

    make install_requires optional; disable in stdeb
    git-svn-id: https://code.astraw.com/motmot/trunk/imops@598 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, Extension, find_packages
 
 import numpy
 
+kws = {}
+if not int(os.getenv( 'DISABLE_INSTALL_REQUIRES','0' )):
+    kws['install_requires'] = [
+        'numpy>=1.0.4',
+        ]
+
 setup(name="motmot.imops",
       description="image format conversion (e.g. between MONO8, YUV422, and RGB)",
       long_description = """
@@ -15,11 +21,10 @@ This is a subpackage of the motmot family of digital image utilities.
       url="http://code.astraw.com/projects/motmot",
       packages = find_packages(),
       namespace_packages = ['motmot'],
-      install_requires = ['numpy>=1.0.4'],
       ext_modules=[Extension(name="motmot.imops.imops",
                              sources=['src/imops.pyx','src/color_convert.c',],
                              include_dirs=[numpy.get_include()],
                              ),
                    ],
       zip_safe = True,
-     )
+      **kws)
