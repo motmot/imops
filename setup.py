b82a2e9ca2486f6e7b4ebefc8c55347242f021ed
Andrew Straw <strawman@astraw.com>
2014-03-28 14:12:51 +0100

    bump version for release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name="motmot.imops",
       long_description = """
 This is a subpackage of the motmot family of digital image utilities.
 """,
-      version="0.5.7",
+      version="0.5.8",
       license="BSD",
       maintainer="Andrew Straw",
       maintainer_email="strawman@astraw.com",
