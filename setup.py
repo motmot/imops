ecd491272ba149b85eafef2355610ccffa87ef1b
Andrew Straw <strawman@astraw.com>
2020-04-07 23:10:19 +0200

    reformat with black

--- setup.py
+++ setup.py
@@ -4,21 +4,26 @@ from Cython.Build import cythonize
 
 import numpy
 
-setup(name="motmot.imops",
-      description="image format conversion (e.g. between MONO8, YUV422, and RGB)",
-      long_description = """
+setup(
+    name="motmot.imops",
+    description="image format conversion (e.g. between MONO8, YUV422, and RGB)",
+    long_description="""
 This is a subpackage of the motmot family of digital image utilities.
 """,
-      version="0.5.9",
-      license="BSD",
-      maintainer="Andrew Straw",
-      maintainer_email="strawman@astraw.com",
-      url="http://code.astraw.com/projects/motmot/imops.html",
-      packages = ['motmot','motmot.imops'],
-      namespace_packages = ['motmot'],
-      ext_modules=cythonize([Extension(name="motmot.imops.imops",
-                             sources=['src/imops.pyx','src/color_convert.c',],
-                             include_dirs=[numpy.get_include()],
-                             ),
-                   ]),
-      )
+    version="0.5.9",
+    license="BSD",
+    maintainer="Andrew Straw",
+    maintainer_email="strawman@astraw.com",
+    url="http://code.astraw.com/projects/motmot/imops.html",
+    packages=["motmot", "motmot.imops"],
+    namespace_packages=["motmot"],
+    ext_modules=cythonize(
+        [
+            Extension(
+                name="motmot.imops.imops",
+                sources=["src/imops.pyx", "src/color_convert.c",],
+                include_dirs=[numpy.get_include()],
+            ),
+        ]
+    ),
+)
