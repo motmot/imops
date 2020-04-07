97c8e2657d4fc4a235ad0d8c80c334c4e627050c
Andrew Straw <strawman@astraw.com>
2020-04-07 23:10:37 +0200

    py3 fixes

--- examples/simple.py
+++ examples/simple.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 # show mono16 format logic
 import numpy
 import motmot.imops.imops as imops
@@ -11,6 +12,6 @@ fin[0, 1] = 255 << 4
 fin2 = numpy.fromstring(fin.tostring(), dtype=numpy.uint8)
 fin2.shape = fin.shape[0], fin.shape[1] * 2
 fout = imops.mono16_to_mono8_middle8bits(fin2)
-print "fin[0,0], fout[0,0]", fin[0, 0], fout[0, 0]
-print "fin[0,1], fout[0,1]", fin[0, 1], fout[0, 1]
-print "fin[0,2], fout[0,2]", fin[0, 2], fout[0, 2]
+print("fin[0,0], fout[0,0]", fin[0, 0], fout[0, 0])
+print("fin[0,1], fout[0,1]", fin[0, 1], fout[0, 1])
+print("fin[0,2], fout[0,2]", fin[0, 2], fout[0, 2])
