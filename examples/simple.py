37e888fb95cb06f29dc894822693ddd36d6afb5d
Andrew Straw <strawman@astraw.com>
2020-04-08 00:55:27 +0200

    remove numpy.fromstring()

--- examples/simple.py
+++ examples/simple.py
@@ -9,7 +9,7 @@ fin = numpy.zeros((h, w), dtype=numpy.uint16)
 fin[0, 0] = 255
 fin[0, 1] = 255 << 4
 
-fin2 = numpy.fromstring(fin.tostring(), dtype=numpy.uint8)
+fin2 = numpy.frombuffer(fin.tobytes(), dtype=numpy.uint8)
 fin2.shape = fin.shape[0], fin.shape[1] * 2
 fout = imops.mono16_to_mono8_middle8bits(fin2)
 print("fin[0,0], fout[0,0]", fin[0, 0], fout[0, 0])
